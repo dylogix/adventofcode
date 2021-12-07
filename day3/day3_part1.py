fff575a99be4a7bff7ab592ee7e821e5c53fbf17
Dominik Jordan <dominik.jordan02@gmail.com>
2021-12-07 22:48:54 +0100

    day 3
    https://adventofcode.com/2021/day/3

---
+++ day3/day3_part1.py
@@ -0,0 +1,53 @@
+data_file = open("day3/data.txt", "r")
+values = data_file.readlines()
+
+def binaryToDecimal(binary):
+     
+    binary1 = binary
+    decimal, i, n = 0, 0, 0
+    while(binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)
+        binary = binary//10
+        i += 1
+    return decimal   
+    
+def get_common(column):
+    count_1 = 0
+    count_0 = 0
+    for value in values:
+        if int(value[column]) == 1:
+            count_1 += 1
+        elif int(value[column]) == 0:
+            count_0 += 1
+    if count_1 < count_0:
+        return '0'
+    else:
+        return '1'
+
+def get_uncommon(column):
+    count_1 = 0
+    count_0 = 0
+    for value in values:
+        if int(value[column]) == 1:
+            count_1 += 1
+        elif int(value[column]) == 0:
+            count_0 += 1
+    if count_1 < count_0:
+        return '1'
+    else:
+        return '0'
+
+gamma_bin = ''
+epsilon_bin = ''
+
+for i in range(0, 12):
+    gamma_bin = gamma_bin + get_common(i)
+    epsilon_bin = epsilon_bin + get_uncommon(i)
+
+gamma = binaryToDecimal(int(gamma_bin))
+epsilon = binaryToDecimal(int(epsilon_bin))
+
+power_consumption = gamma * epsilon
+
+print('Power Consumption: ' + str(power_consumption)) # solution: 2595824
---
+++ day3/day3_part2.py
@@ -0,0 +1,74 @@
+data_file = open("day3/data.txt", "r")
+values = data_file.readlines()
+
+def binaryToDecimal(binary):
+     
+    binary1 = binary
+    decimal, i, n = 0, 0, 0
+    while(binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)
+        binary = binary//10
+        i += 1
+    return decimal   
+
+def compare_and_filter_common(list, column):
+    filtered_values = []
+
+    count_1 = 0
+    count_0 = 0
+    for value in list:
+        if int(value[column].strip()) == 1:
+            count_1 += 1
+        elif int(value[column].strip()) == 0:
+            count_0 += 1
+
+    if count_1 < count_0:
+        common = 0
+    else:
+        common = 1
+
+    for value in list:
+        if int(value[column].strip()) == common:
+            filtered_values.append(value)
+
+    return filtered_values
+
+def compare_and_filter_uncommon(list, column):
+    filtered_values = []
+
+    count_1 = 0
+    count_0 = 0
+    for value in list:
+        if int(value[column].strip()) == 1:
+            count_1 += 1
+        elif int(value[column].strip()) == 0:
+            count_0 += 1
+
+    if count_1 < count_0:
+        common = 1
+    else:
+        common = 0
+    
+    for value in list:
+        if int(value[column].strip()) == common:
+            filtered_values.append(value)
+
+    return filtered_values
+
+tmp = values
+tmp2 = values
+for i in range(0, 12):
+    if len(tmp) > 1:
+        tmp = compare_and_filter_common(tmp, i)
+    
+    if len(tmp2) > 1:
+        tmp2 = compare_and_filter_uncommon(tmp2, i)
+
+
+oxygen_generator_rating = binaryToDecimal(int(tmp[0]))
+co2_scrubber_rating = binaryToDecimal(int(tmp2[0]))
+
+life_support_rating = oxygen_generator_rating * co2_scrubber_rating
+
+print('Life support rating: ' + str(life_support_rating)) # solution: 2135254
